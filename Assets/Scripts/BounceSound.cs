076924f60c9abf7377e1086f1dfd07e7a63f8ae2
Wendel Scardua <wendelscardua@gmail.com>
2016-12-22 17:46:49 -0200

    Add bounce effect

---
+++ Assets/Scripts/BounceSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceSound : MonoBehaviour {
+    public AudioSource source;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float magnitude = collision.relativeVelocity.magnitude;
+        magnitude = Mathf.Clamp(magnitude / 10.0f, 0.0f, 1.0f);
+        source.PlayOneShot(source.clip, magnitude);
+    }
+}
