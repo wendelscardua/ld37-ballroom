02b6e16ff1fe8f559c335a86ee2d05568619ed58
Wendel Scardua <wendelscardua@gmail.com>
2016-12-22 00:28:35 -0200

    Add simple main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -32,12 +32,12 @@ public class GameManager : MonoBehaviour {
     }
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         UpdateScore();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (balls > 0)
         {
             spawnCooldown -= Time.deltaTime;
